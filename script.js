e74c11912d9fb01b8b32e972745015f9fcbaf814
Clarissa Ardon <klary.ardon@gmail.com>
2022-01-06 00:30:51 +0100

    mousemove event listener and event object

--- script.js
+++ script.js
@@ -20,6 +20,16 @@ window.addEventListener("resize", function () {
   flowField.animate();
 });
 
+const mouse = {
+  x: 0,
+  y: 0,
+};
+window.addEventListener("mousemove", function (e) {
+  mouse.x = e.x;
+  mouse.y = e.y;
+  console.log(e);
+});
+
 class FlowFieldEffect {
   //Which canvas element we want to draw on
   #ctx;
@@ -28,6 +38,7 @@ class FlowFieldEffect {
   constructor(ctx, width, height) {
     this.#ctx = ctx;
     this.#ctx.strokeStyle = "white";
+    this.#ctx.lineWidth = 5;
     this.#width = width;
     this.#height = height;
     this.angle = 0;
@@ -36,17 +47,14 @@ class FlowFieldEffect {
     const length = 300;
     this.#ctx.beginPath();
     this.#ctx.moveTo(x, y);
-    this.#ctx.lineTo(x + length, y + length);
+    this.#ctx.lineTo(mouse.x, mouse.y);
     this.#ctx.stroke();
   }
   animate() {
     this.angle += 0.1;
-    this.#ctx.clearRect(0, 0, this.#width, this.#height);
-    this.#draw(
-      this.#width / 2 + Math.sin(this.angle) * 100,
-      this.#height / 2 + Math.cos(this.angle) * 100
-    );
-    console.log("animating");
+    // this.#ctx.clearRect(0, 0, this.#width, this.#height);
+    this.#draw(this.#width / 2, this.#height / 2);
+    // console.log("animating");
     flowFieldAnimation = requestAnimationFrame(this.animate.bind(this));
   }
 }
