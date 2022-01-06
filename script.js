42600aa9d08668b817294e8d4e5cc6eba30c9148
Clarissa Ardon <klary.ardon@gmail.com>
2022-01-07 00:22:06 +0100

    timestamps and delta time

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ window.onload = function () {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
   flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height);
-  flowField.animate();
+  flowField.animate(0);
 };
 
 window.addEventListener("resize", function () {
@@ -17,7 +17,7 @@ window.addEventListener("resize", function () {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
   flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height);
-  flowField.animate();
+  flowField.animate(0);
 });
 
 const mouse = {
@@ -27,7 +27,6 @@ const mouse = {
 window.addEventListener("mousemove", function (e) {
   mouse.x = e.x;
   mouse.y = e.y;
-  console.log(e);
 });
 
 class FlowFieldEffect {
@@ -37,11 +36,14 @@ class FlowFieldEffect {
   #height;
   constructor(ctx, width, height) {
     this.#ctx = ctx;
-    this.#ctx.strokeStyle = "white";
+    this.#ctx.strokeStyle = "purple";
     this.#ctx.lineWidth = 5;
     this.#width = width;
     this.#height = height;
     this.angle = 0;
+    this.lastTime = 0;
+    this.interval = 1000/60;
+    this.timer = 0;
   }
   #draw(x, y) {
     const length = 300;
@@ -50,11 +52,17 @@ class FlowFieldEffect {
     this.#ctx.lineTo(mouse.x, mouse.y);
     this.#ctx.stroke();
   }
-  animate() {
-    this.angle += 0.1;
-    // this.#ctx.clearRect(0, 0, this.#width, this.#height);
-    this.#draw(this.#width / 2, this.#height / 2);
-    // console.log("animating");
+  animate(timeStamp) {
+    const deltaTime = timeStamp - this.lastTime;
+    this.lastTime = timeStamp;
+    if (this.timer > this.interval) {
+        this.angle += 0.1;
+        this.#ctx.clearRect(0, 0, this.#width, this.#height);
+        this.#draw(this.#width / 2, this.#height / 2);
+        this.timer = 0;
+    } else {
+        this.timer += deltaTime;
+    }
     flowFieldAnimation = requestAnimationFrame(this.animate.bind(this));
   }
 }
