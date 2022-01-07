cd618a176ecf31407526517a144be24aeaaf9984
Clarissa Ardon <klary.ardon@gmail.com>
2022-01-07 20:13:56 +0100

    spiral animation

--- script.js
+++ script.js
@@ -36,16 +36,18 @@ class FlowFieldEffect {
   #height;
   constructor(ctx, width, height) {
     this.#ctx = ctx;
-    this.#ctx.lineWidth = 0.5;
+    this.#ctx.lineWidth = 1;
     this.#width = width;
     this.#height = height;
     this.lastTime = 0;
     this.interval = 1000 / 60;
     this.timer = 0;
-    this.cellSize = 4;
+    this.cellSize = 15;
     this.gradient;
     this.#createGradient();
     this.#ctx.strokeStyle = this.gradient;
+    this.radius = 0;
+    this.vr = 0.03;
   }
   #createGradient() {
     this.gradient = this.#ctx.createLinearGradient(
@@ -62,10 +64,13 @@ class FlowFieldEffect {
     this.gradient.addColorStop("0.9", "#ffff33");
   }
   #drawLine(angle, x, y) {
-    const length = 300;
+    const length = 15;
     this.#ctx.beginPath();
     this.#ctx.moveTo(x, y);
-    this.#ctx.lineTo(x + Math.cos(angle) * 30, y + Math.sin(angle) * 30);
+    this.#ctx.lineTo(
+      x + Math.cos(angle) * length,
+      y + Math.sin(angle) * length
+    );
     this.#ctx.stroke();
   }
   animate(timeStamp) {
@@ -73,10 +78,12 @@ class FlowFieldEffect {
     this.lastTime = timeStamp;
     if (this.timer > this.interval) {
       this.#ctx.clearRect(0, 0, this.#width, this.#height);
+      this.radius += this.vr;
+      if (this.radius > 5 || this.radius < -5) this.vr *= -1;
 
       for (let y = 0; y < this.#height; y += this.cellSize) {
         for (let x = 0; x < this.#width; x += this.cellSize) {
-          const angle = (Math.cos(x * 0.01) + Math.sin(y * 0.01)) * 3;
+          const angle = (Math.cos(x * 0.01) + Math.sin(y * 0.01)) * this.radius;
           this.#drawLine(angle, x, y);
         }
       }
