2aa4d81ebcb7c02a8d928b05dbbd213d6bfdc16c
Clarissa Ardon <klary.ardon@gmail.com>
2022-01-07 01:14:50 +0100

    grid in html

--- script.js
+++ script.js
@@ -40,28 +40,32 @@ class FlowFieldEffect {
     this.#ctx.lineWidth = 5;
     this.#width = width;
     this.#height = height;
-    this.angle = 0;
     this.lastTime = 0;
-    this.interval = 1000/60;
+    this.interval = 1000 / 60;
     this.timer = 0;
+    this.cellSize = 35;
   }
-  #draw(x, y) {
+  #drawLine(x, y) {
     const length = 300;
     this.#ctx.beginPath();
     this.#ctx.moveTo(x, y);
-    this.#ctx.lineTo(mouse.x, mouse.y);
+    this.#ctx.lineTo(x + 25, y + 10);
     this.#ctx.stroke();
   }
   animate(timeStamp) {
     const deltaTime = timeStamp - this.lastTime;
     this.lastTime = timeStamp;
     if (this.timer > this.interval) {
-        this.angle += 0.1;
-        this.#ctx.clearRect(0, 0, this.#width, this.#height);
-        this.#draw(this.#width / 2, this.#height / 2);
-        this.timer = 0;
+      this.#ctx.clearRect(0, 0, this.#width, this.#height);
+
+      for (let y = 0; y < this.#height; y += this.cellSize) {
+        for (let x = 0; x < this.#width; x += this.cellSize) {
+          this.#drawLine(x, y);
+        }
+      }
+      this.timer = 0;
     } else {
-        this.timer += deltaTime;
+      this.timer += deltaTime;
     }
     flowFieldAnimation = requestAnimationFrame(this.animate.bind(this));
   }
